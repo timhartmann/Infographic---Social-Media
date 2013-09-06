ae81167bdf7e56ac4e11131c457f916146742823
Tim Hartmann <tim.hartmann93@gmail.com>
2013-09-06 10:55:57 +0200

    add tasks, tools and structure

---
+++ Gruntfile.js
@@ -0,0 +1,97 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    watch: {
+      files: [
+        'src/**'
+      ],
+      tasks: ['default']
+    },
+    sass: {
+      build: {
+        options: {
+          style: 'expanded',
+          compass: true
+        },
+        files: [
+          {
+            expanded: true,
+            cwd: 'src/assets',
+            src: ['src/assets/scss/application.scss'],
+            dest: 'build/assets/stylesheets/application.css',
+            filter: 'isFile'
+          }
+        ]
+      }
+    },
+    clean: {
+      files: [
+        'build/assets/stylesheets/application.css'
+      ]
+    },
+    copy: {
+      main: {
+        expand: true,
+        src: ['src/assets/fonts/*'],
+        dest: 'build/assets/fonts',
+        filter: 'isFile',
+        flatten: true
+      }
+    },
+    cssmin: {
+      minify: {
+        expand: true,
+        cwd: 'build/assets/stylesheets/',
+        src: [
+          'application.css'
+        ],
+        dest: 'build/assets/stylesheets/',
+        ext: '.min.css'
+      }
+    },
+    concat: {
+      build: {
+        src: [
+          'src/assets/js/application.js'
+        ],
+        dest: 'build/assets/javascript/application.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'build/assets/javascript/application.js',
+        dest: 'build/assets/javascript/application.min.js'
+      }
+    },
+    bumpup: ['package.json', 'bower.json'],
+    tagrelease: {
+      file: 'package.json',
+      commit:  false,
+      message: 'Release %version%',
+      prefix:  '',
+      annotate: false
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-tagrelease');
+  grunt.loadNpmTasks('grunt-bumpup');
+
+  // Default task(s).
+  grunt.registerTask('default', ['sass', 'copy', 'concat']);
+  grunt.registerTask('release', function (type) {
+    type = type ? type : 'patch';
+    grunt.task.run('default');
+    grunt.task.run('cssmin');
+    grunt.task.run('clean');
+    grunt.task.run('uglify');
+    grunt.task.run('bumpup:' + type);
+    grunt.task.run('tagrelease');
+  });
+};
